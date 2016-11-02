d7b2674a72c9e1b0a70aa1e5496dabccaf790010
giopl <giopl4@gmail.com>
2016-11-02 14:06:06 +0400

    adding app_option table

---
+++ CmonitorDB/dbo/Tables/app_option.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[app_option] (
+    [id]    INT            IDENTITY (1, 1) NOT NULL,
+    [field] NVARCHAR (100) NOT NULL,
+    [name]  NVARCHAR (100) NOT NULL,
+    [value] NVARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
